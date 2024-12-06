9c0372a69d29da3514d3d68827b630bac84e3bf9
PompaTy <tyler.pompa@gmail.com>
2024-12-06 09:37:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 from nanonav import BLE, NanoBot
 import time
 
-### test motors and encoders ###
-
-# Create a NanoBot object
-robot = NanoBot()
-current_location = Point(start_row, start_col)
-orientation = 0  #0 means up, 1 means right, 2 means down, 3 means left
-has_gold = False
-wumpus_dead = False
-wumpus_location = None  #Will be a Point when discovered
-
-class SignalType(Enum):
-    GLIMMER = "Glimmer"
-    BREEZE = "Breeze"
-    STENCH = "Stench"
-    GOLD = "Gold"
-    NONE = "None"
 
 #This is the Point class to represent a square in the grid
 class Point:
@@ -30,14 +14,27 @@ class Point:
     def __repr__(self):
         return f"Point(row={self.row}, col={self.col}, safe={self.safe}, confirmed={self.confirmed}, gold={self.gold})"
 
+# Create a NanoBot object
+robot = NanoBot()
+current_location = Point(3, 0)
+orientation = 0  #0 means up, 1 means right, 2 means down, 3 means left
+has_gold = False
+wumpus_dead = False
+wumpus_location = None  #Will be a Point when discovered
+ble = BLE(name="Test Name")
+ble.send(0)
+
+
+
+
 #This is the signal class to represent the signal received from a square
 class Signal:
-    def __init__(self, point: Point, signal_type: SignalType):
+    def __init__(self, point: Point, signal_type):
         self.point = point
         self.signal_type = signal_type
 
     def __repr__(self):
-        return f"Signal(point={self.point}, signal_type={self.signal_type.name})"
+        return f"Signal(point={self.point}, signal_type={self.signal_type})"
 
 def move(self, direction: str):
     if direction == "up":
@@ -72,35 +69,37 @@ def logSignal():
      time.sleep(5)
 
      #read from BLE and convert to int
-     response = int(ble.read(),2)
+     response = ble.read()
+     if response != None:
+         print(response)
 
-     #check for signals
-     breeze = 1 & response != 0
-     stench = 2 & response != 0
-     glimmer = 4 & response != 0
-     gold = 8 & response != 0
+         #check for signals
+         breeze = response & 1 != 0
+         stench = response & 2 != 0
+         glimmer = response & 4 != 0
+         gold = response & 8 != 0
 
-     signals = [breeze, stench, glimmer, gold]
-     signal_types = [SignalType.BREEZE, SignalType.STENCH, SignalType.GLIMMER, SignalType.GOLD]
-
-     #check if there are any signals
-     if any(signals):
-         #create signal object based on received signals
-         for i in range(4):
-             if signals[i]:
-                 point = grid[robot.current_location.row][robot.current_location.col]
-                 signal = Signal(point,signal_types[i])
-                 unconfirmed_squares.append(signal)
-     else:
-         row, col = robot.current_location.row, robot.current_location.col
-         directions = [(-1,0), (1,0), (0,-1), (0,1)]
-         for i in range(len(directions)):
-             r = row + directions[i][0]
-             c = col + directions[i][1]
-             if 0 <= r < 4 and 0 <= c < 4:
-                 grid[r][c].safe = True
-                 grid[r][c].confirmed = True
-                 next_squares.append(grid[r][c])
+         signals = [breeze, stench, glimmer, gold]
+         signal_types = ["BREEZE", "STENCH", "GLIMMER", "GOLD"]
+         print(signals)
+         # #check if there are any signals
+         # if any(signals):
+         #     #create signal object based on received signals
+         #     for i in range(4):
+         #         if signals[i]:
+         #             point = grid[robot.current_location.row][robot.current_location.col]
+         #             signal = Signal(point,signal_types[i])
+         #             unconfirmed_squares.append(signal)
+         # else:
+         #     row, col = robot.current_location.row, robot.current_location.col
+         #     directions = [(-1,0), (1,0), (0,-1), (0,1)]
+         #     for i in range(len(directions)):
+         #         r = row + directions[i][0]
+         #         c = col + directions[i][1]
+         #         if 0 <= r < 4 and 0 <= c < 4:
+         #             grid[r][c].safe = True
+         #             grid[r][c].confirmed = True
+         #             next_squares.append(grid[r][c])
 
 
 #The following is the grid which is a 4x4 list of lists containing Points
@@ -113,66 +112,32 @@ next_squares = []
 unconfirmed_squares = []
 
 #This would instantiate the robot at the bottom-left corner (Im not sure how to make it so that it is 0,0 like we established, we could do a similar thing like 3,0 like what I originally proposed but feel free to change it!:))
-robot = Robot(start_row=, start_col=)
-
-while(current_location != Point(0,0) and has_gold){
-    if(len(next_squares) == 0){
-        if(wumpus_location != None and !wumpus_dead){
-            killWumpus()
-        } else{
-            success = computePermutations()
-            if(!success){
-                moveTo(Point(0,0))
-            }
-        }
-    }
-    else{
-        moveTo(next_squares.pop())
-        logSignal()
-    }
-}
-
-
-
-
-
-# Move forward for 2 seconds
-print(f'encoder 1 start: {robot.get_enc1()}')
-robot.m1_forward(30)
-robot.m2_forward(30)
-time.sleep(2)
-print(f'encoder 1 end: {robot.get_enc1()}')
-
-# Stop
-robot.stop()
-time.sleep(2)
-
-# Move backward for 2 seconds
-print(f'encoder 2 start: {robot.get_enc2()}')
-robot.m1_backward(30)
-robot.m2_backward(30)
-time.sleep(2)
-print(f'encoder 2 end: {robot.get_enc2()}')
-
-# Stop
+# robot.set_enc1(0)
+# robot.set_enc2(0)
+while robot.get_enc1() > -100:
+    robot.m1_forward(-30)
+    robot.m2_forward(-30)
+    print(robot.get_enc1())
 robot.stop()
 
-### test Bluetooth ###
+# while(current_location != Point(0,0) and has_gold):
+#     if(len(next_squares) == 0):
+#         if wumpus_location != None and not wumpus_dead:
+#             killWumpus()
+#         else:
+#             success = computePermutations()
+#             if not success:
+#                 moveTo(Point(0,0))
 
-# Create a Bluetooth object
-ble = BLE(name="Test Name")
 
-ble.send(43)
-response = ble.read()
-# wait until something changes, indicating a response
-while response == 43:
-    response = ble.read()
-    time.sleep(0.5)
 
-print("Received: ", response)
+#     else:
+#         moveTo(next_squares.pop())
+#         logSignal()
 
-### test ir sensors ###
-while True:
-    print(f'left: {robot.ir_left()}    right: {robot.ir_right()}')
-    time.sleep(0.5)
+
+
+
+
+
 
